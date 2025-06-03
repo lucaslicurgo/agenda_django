367b8d3bc5f798eefac17082a4cf52adc486ae9c
Lucas Licurgo <lucasclicurgo@gmail.com>
2025-06-03 17:33:54 -0300

    Ordenando lista

--- contact/models.py
+++ contact/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Contact(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50, blank=True)
+    phone = models.CharField(max_length=50)
+    email = models.EmailField(max_length=254)
+    created_date = models.DateTimeField(default=timezone.now)
+    description = models.TextField()
+
+    def __str__(self) -> str:
+        return f'[{self.id}] {self.first_name} {self.last_name}'
+
