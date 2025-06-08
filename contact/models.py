f8cea30be84627c9f966a7a25b9d3e3245e57c4d
Lucas Licurgo <lucasclicurgo@gmail.com>
2025-06-08 14:59:53 -0300

    Ajeitando .env

--- contact/models.py
+++ contact/models.py
@@ -1,10 +1,11 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
 
 class Category(models.Model):
     class Meta:
         verbose_name = 'Category'
-        verbose_name_pluaral = 'Categories'
+        verbose_name_plural = 'Categories'
 
     name = models.CharField(max_length=50)
 
@@ -21,6 +22,11 @@ class Contact(models.Model):
     show = models.BooleanField(default=True)
     picture = models.ImageField(blank=True, upload_to='pictures/%Y/%m/')
     category = models.ForeignKey(Category, on_delete=models.SET_NULL, blank=True, null=True)
+    owner = models.ForeignKey(
+        User,
+        on_delete=models.SET_NULL,
+        blank=True, null=True
+    )
 
     def __str__(self) -> str:
         return f'[{self.id}] {self.first_name} {self.last_name}'
